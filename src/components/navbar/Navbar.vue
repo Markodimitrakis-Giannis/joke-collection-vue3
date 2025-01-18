f71980630e5c04447602fe233afa3741123cac41
Markodimitrakis Giannis <62894405+Markodimitrakis-Giannis@users.noreply.github.com>
2025-01-18 11:09:28 +0200

    - Navbar first edition with links completed & first setup of routes

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,15 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <nav
+    class="bg-gradient-to-r from-purple-900 via-purple-600 to-purple-400 border-b border-blue-50"
+  >
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="flex h-20 items-center justify-between">
+        <div class="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+          <RouterLink to="/" class="flex flex-shrink-0 items-center mr-4">
+            <img class="h-10 w-auto" src="@/assets/images/joker-logo.svg" alt="joker_img" />
+            <span class="hidden md:block text-white text-2xl font-bold ml-2">Jokes App</span>
+          </RouterLink>
+          <div class="flex flex-1"></div>
+          <div>
+            <div class="flex space-x-2">
+              <RouterLink
+                to="/"
+                activeclass="bg-green-900 text-white"
+                class="text-gray-100 hover:text-black transition-colors duration-200 rounded-md px-3 py-2 font-semibold"
+                >Home</RouterLink
+              >
+              <RouterLink
+                to="/jokes"
+                class="text-gray-100 hover:text-black transition-colors duration-200 rounded-md px-3 py-2 font-semibold"
+                >Jokes</RouterLink
+              >
+              <RouterLink
+                to="/collection"
+                class="text-gray-100 hover:text-black transition-colors duration-200 rounded-md px-3 py-2 font-semibold"
+                >My Collection</RouterLink
+              >
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div></div>
-  </header>
-
-  <RouterView />
+  </nav>
 </template>
---
+++ src/views/Collection.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-full">
+    <div class="bg-white p-4 rounded-lg shadow-lg">
+      <h1 class="text-2xl font-bold text-center">Collection</h1>
+    </div>
+  </div>
+</template>
